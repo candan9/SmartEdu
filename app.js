4fefbe52c6b385d58b067ab8fc9bfc46384a2fe0
emre candan <wastorist@gmail.com>
2021-10-14 11:49:57 +0300

    ejs init

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//temp engine
+
+app.set('view engine','ejs');
+
+//MIDDLEWARE
+
+app.use(express.static("public"));
+
+//Route
 app.get('/', (req, res) => {
-  res.statusCode(200).send('INDEX SAYFASI');
+  res.status(200).render('index',{
+      page_name:"index"
+  });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name:"about"
+    });
+  });
+  
 
 const port = 3000;
 app.listen(port, () => {
