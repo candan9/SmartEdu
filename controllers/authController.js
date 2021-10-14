e598a6ff9a0ead2b1b8cd43aaec5090427f29bb3
emre candan <wastorist@gmail.com>
2021-10-14 19:08:03 +0300

    session

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
-
 const bcrypt = require('bcrypt');
+
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -26,7 +26,8 @@ exports.loginUser =  (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             // USER SESSION
-            res.status(200).send('YOU ARE LOGGED IN');
+            req.session.userID = user._id;
+            res.status(200).redirect('/');
           }
         });
       }
