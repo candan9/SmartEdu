3139901d72ff1bb9ae6c5fd5d080c81be4e9cb11
emre candan <wastorist@gmail.com>
2021-10-14 14:09:03 +0300

    courses listed

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 const app = express();
+
+//connect db
+
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected!');
+  });
 
 //temp engine
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 
-app.use(express.static("public"));
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //Route
-app.get('/', (req, res) => {
-  res.status(200).render('index',{
-      page_name:"index"
-  });
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).render('about',{
-        page_name:"about"
-    });
-  });
-  
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
