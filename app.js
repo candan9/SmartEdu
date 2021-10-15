594c2860eacc7f90db1da6016ccc38aff0b0df5c
emre candan <wastorist@gmail.com>
2021-10-15 17:35:42 +0300

    kurs silme

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const flash = require('connect-flash')
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
@@ -15,9 +16,10 @@ mongoose
   .connect('mongodb://localhost/smartedu-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  
   })
   .then(() => {
-    console.log('DB Connected!');
+    console.log('DB Connected Successfully');
   });
 
 //temp engine
@@ -40,12 +42,21 @@ app.use(session({
   saveUninitialized: true,
   store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
 }))
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 app.use(flash());
 app.use((req, res, next)=> {
   res.locals.flashMessages = req.flash();
   next();
 })
-
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 
 
